7290b940fce2866e2e0c1d361eea008990fce4f4
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-28 18:30:39 +0300

    Add input validation for Author and Post models
    Added name and phone number validators to the Author class.
    
    Updated title, content, summary, and category validators in the Post class.
    
    These changes ensure that data integrity is maintained in the database.

--- server/models.py
+++ server/models.py
@@ -4,21 +4,32 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    # Add validators 
+    
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Authors name is not here hence required')
+        if Author.query.filter(db.func.lower(Author.name) == name.lower()).first():
+            raise ValueError('Author with the same name already exists in the database.')
+        return name
+    @validates('phone_number')
+    def validate_phonenumber(self,key,phone_number):
+        if phone_number and (len(phone_number) != 10 or not phone_number.isdigit()):
+            raise ValueError('Authors phone number is not valid')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
@@ -26,9 +37,33 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError('Post title is required.')
+        clickbait_words = ["unbelievable", "mind-blowing", "exposed", "mysterious", "incredible"]
+        if any(word in title.lower() for word in clickbait_words):
+            raise ValueError("Title should not contain clickbait.")
+        return title
 
-    # Add validators  
-
-
+    @validates('content')
+    def validate_content(self, key, content):
+        if content and len(content) < 250:
+            raise ValueError('Post content must be at least 250 characters long.')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError('Post summary cannot go beyond 250 characters.')
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('Post category must be either Fiction or Non-Fiction.')
+        return category
+    
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
